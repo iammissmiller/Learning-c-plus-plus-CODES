a192862ab2f670d4c193678d48dbebb0429a1eb6
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-11 13:11:14 +0530

    Add files via upload

---
+++ variableshadowing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+//global variable 
+int a = 5;
+
+int main() {
+
+//local variable with same name as that of
+//global variable 
+
+int a = 100;
+
+//accesing a 
+
+cout << a ;
+
+return 0;
+
+
+}
