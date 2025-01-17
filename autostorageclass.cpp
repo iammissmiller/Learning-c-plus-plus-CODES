d8e280b0576a032469b5d241c5aca3b503dd7676
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-17 23:14:12 +0530

    Add files via upload

---
+++ autostorageclass.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+    int a = 32;
+    float b = 3.2;
+    string c = "GeeksforGeeks";
+
+    /
+    cout << a << " \n";
+    cout << b << " \n";
+      cout << c << " \n";
+
+    return 0;
+}
