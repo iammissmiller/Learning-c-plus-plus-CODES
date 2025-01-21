1da0c2c23984980d4485d78175e1973de56ea144
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-21 23:34:28 +0530

    cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // Variable to store data given by cin
+    int a;
+
+    // Take input using cin
+    cin >> a;
+
+    cout << a;
+    return 0;
+}
