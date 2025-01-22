f908c8a311c22f8929b868151888249ac2d9fa72
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-22 23:18:19 +0530

    cin3.cpp

---
+++ cin3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    
+    int age ;
+    
+    cin >> name >> age ;
+    
+    cout << name << endl;
+    
+    cout << age << endl;
+    
+    return 0;
+    
+}
