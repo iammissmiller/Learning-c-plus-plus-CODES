a3b911aa8c61c43441203d3af7409c39293da0e1
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-23 23:44:00 +0530

    cin.getline().cpp

---
+++ cin.getline().cpp
@@ -0,0 +1,15 @@
+// C++ program to illustrate the use of cin.getline
+#include <iostream>
+using namespace std;
+
+int main() {
+    char name[5];
+
+    // Reads stream of 3 characters
+    cin.getline(name, 3);
+
+    // Print output
+    cout << name << endl;
+
+    return 0;
+}
