7eca9d3f9b08576d6c7545256a385b6aa3a4a46c
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-25 23:47:16 +0530

    cout_multiplevariable.cpp

---
+++ cout_multiplevariable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 42;
+    string s = "The answer is ";
+
+      // Printing both n and s
+    cout << s << n;
+  
+    return 0;
+}
