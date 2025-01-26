1db62171424e61a45a719069c166c83d5e3e7b85
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-26 19:25:54 +0530

    cout2.c

---
+++ cout2.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char s[] = "Welcome at GFG";
+    char c = 'e';
+
+    // Print first 6 characters
+    cout.write(s, 6);
+
+    // Print the character c
+    cout.put(c);
+  
+    return 0;
+}
