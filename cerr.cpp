70d1e5fb6ef5e3901e27579228a9bf851a20452b
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-01-28 22:06:50 +0530

    cerr.cpp

---
+++ cerr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // Printing error
+    cerr << "Welcome to GfG! :: cerr\n";
+  
+      // Printing using cout
+    cout << "Welcome to GfG! :: cout";
+    return 0;
+}
