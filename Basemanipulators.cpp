31dee1db257d9e8efbeb63daa1c33a102a8c7965
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-02-02 21:53:48 +0530

    Basemanipulators.cpp

---
+++ Basemanipulators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 42;
+
+    // Output in hexadecimal base
+    cout << hex << n << endl;
+
+    // Output in decimal base
+    cout << dec << n << endl;
+
+    // Output in octal base
+    cout << oct << n;
+
+    return 0;
+}
