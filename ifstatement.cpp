b090a6d78331dc9c072fa26110159ff37a7ed0d0
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-02-03 20:28:59 +0530

    ifstatement.cpp

---
+++ ifstatement.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+  
+int main()  { 
+    int i = 10; 
+  
+      // If statement
+    if (i < 15) { 
+        cout << "10 is less than 15"; 
+    } 
+  
+    return 0;
+}
