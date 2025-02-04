04747fd0415d2a64b050d204b0a49900806ed11e
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-02-04 22:36:11 +0530

    ifstatement2.c

---
+++ ifstatement2.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 8;
+
+    // Condition to check for even number
+    if (n % 2 == 0)
+        cout << " Even";        
+
+    return 0;
+}
