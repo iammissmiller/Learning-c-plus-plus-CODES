986f28ee15a7fa0f3f75f1510140d0138676919a
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-02-05 22:57:43 +0530

    ifstatementrange.cpp

---
+++ ifstatementrange.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    // Check whether n is the range [0, 10]
+    if (n <= 10 && n >= 0) {
+        cout << "In range";
+    }
+
+    return 0;
+}
