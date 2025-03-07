75b38e0ef39e75794e0c59340eb0809207928a25
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-03-07 23:03:46 +0530

    Stringinfunction.cpp

---
+++ Stringinfunction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Taking string as argument
+void print(string s) {
+    cout << s;
+    return;
+}
+
+int main() {
+    string s = "GeeksforGeeks";
+    
+    // Passing string
+    print(s);
+    return 0;
+}
