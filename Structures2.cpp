d3131fbfeaf584df1b2a59dd3a9327e2ee9ebee5
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-03-10 22:34:53 +0530

    Structures2.cpp

---
+++ Structures2.cpp
@@ -0,0 +1,10 @@
+struct Point {
+    int x, y;
+};
+
+int main()
+{
+    // A valid initialization. member x gets value 0 and y
+    // gets value 1.  The order of declaration is followed.
+    struct Point p1 = { 0, 1 };
+}
