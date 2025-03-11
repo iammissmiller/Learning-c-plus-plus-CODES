484c37efa6df1519e0d2f927650f34f61c4d89e2
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-03-11 19:49:54 +0530

    Union.cpp

---
+++ Union.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Creating a union
+union geek {
+    int age;
+    char grade;
+    float GPA;
+};
+
+int main() {
+
+    // Defining a union variable
+    union geek student1;
+
+    // Assigning values to data member of union geek and
+    // printing the values of data members
+    student1.age = 25;
+    cout << student1.age << endl;
+    student1.grade = 'B';
+    cout << student1.grade << endl;
+    student1.GPA = 4.5;
+    cout <<  student1.GPA;
+    return 0;
+}
