1963cfcce4e51ecfa5e5fbc5458ba578085c241f
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-03-23 21:20:48 +0530

    Polymorphism.cpp

---
+++ Polymorphism.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Geeks {
+public:
+    
+    // Function to add two integers
+    void add(int a, int b) {
+        cout << "Integer Sum = " << a + b
+        << endl;
+    }
+    
+    // Function to add two floating point values
+    void add(double a, double b) {
+        cout << "Float Sum = " << a + b
+        << endl ;
+    }
+};
+
+int main() {
+    Geeks gfg;
+    
+    // add() called with int values
+    gfg.add(10, 2);
+
+    // add() called with double value
+    gfg.add(5.3, 6.2);
+
+    return 0;
+}
