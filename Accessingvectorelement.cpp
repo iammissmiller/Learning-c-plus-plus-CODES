88901fed1d40297fe255beb2bd135014683a4354
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-04-07 23:33:37 +0530

    Accessingvectorelement.cpp

---
+++ Accessingvectorelement.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<char> v = {'a', 'c', 'f', 'd', 'z'};
+
+    // Accessing and printing values
+  	cout << v[3] << endl;
+  	cout << v.at(2);
+  
+    return 0;
+}
