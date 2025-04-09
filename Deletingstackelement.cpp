c631fe9fa08f3c2c5a8cc2ac00cd141c46ec8ff5
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-04-09 23:31:16 +0530

    Deletingstackelement.cpp

---
+++ Deletingstackelement.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    
+    // Deleting top element
+    st.pop();
+    
+    while(!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
+}
