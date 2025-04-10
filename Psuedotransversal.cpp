ce202ff7760d07bcbda067dadb267b6aae3714f4
iammissmiller <139339934+iammissmiller@users.noreply.github.com>
2025-04-10 23:13:10 +0530

    Psuedotransversal.cpp

---
+++ Psuedotransversal.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    
+    // Create a copy
+    stack<int> temp(st);
+    
+    while(!temp.empty()) {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    return 0;
+}
